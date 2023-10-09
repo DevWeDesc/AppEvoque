93c1b7df2eb840cf7dbc162b306b47f31ee8c8a2
ViniHGV <ixxvinicius@gmail.com>
2023-10-09 09:18:43 -0300

    Criando App

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,93 +1,123 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput,  StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
 
 export interface LoginProps {
   navigation: StackNavigationProp<any>;
 }
 
 export default function Login({ navigation }: LoginProps) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
     console.log(`Email: ${email}, Senha: ${password}`);
-    navigation.navigate('Home');
+    navigation.navigate("Home");
   };
 
-  
   return (
-    
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
       <Image
-        source={require('../../assets/images/evoque.png')}
+        source={require("../../assets/images/evoque.png")}
         style={styles.image}
       />
-      <View>      
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={setEmail}
-        value={email}
-        placeholderTextColor={'#fff'}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        secureTextEntry
-        onChangeText={setPassword}
-        value={password}
-        placeholderTextColor={'#fff'}
-      />
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={setEmail}
+          value={email}
+          placeholderTextColor={"#fff"}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          secureTextEntry
+          onChangeText={setPassword}
+          value={password}
+          placeholderTextColor={"#fff"}
+        />
         <TouchableOpacity style={styles.button} onPress={handleLogin}>
           <Text style={styles.textButton}>Entrar</Text>
         </TouchableOpacity>
+        <View style={styles.loginOther}>
+          <View style={styles.viewOu}></View>
+          <Text style={styles.textLogout}>OU</Text>
+          <View style={styles.viewOu}></View>
+        </View>
+        <Text style={styles.textLogout}>
+          Não tem mais conta?{" "}
+          <Text style={styles.textLogoutCad}>Cadastre-se</Text>
+        </Text>
       </View>
+      <StatusBar style="light" translucent />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  loginOther: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  viewOu: { height: 1, backgroundColor: "white", width: "33%", marginTop: 15 },
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    textTransform: 'uppercase',
-    textAlign: 'center',
+    textTransform: "uppercase",
+    textAlign: "center",
     fontSize: 32,
     marginBottom: 40,
-    color: '#fff',
+    color: "#fff",
     letterSpacing: 3,
   },
   input: {
-    color: '#fff',
+    backgroundColor: "#424242",
+    color: "#fff",
     width: 300,
-    height: 40,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 100,
+    height: 50,
+    borderRadius: 8,
     marginBottom: 20,
     paddingLeft: 10,
   },
   image: {
-    width: 250,
-    height: 250,
-    marginBottom: 200,
+    width: 150,
+    height: 150,
+    marginBottom: 30,
   },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
-    backgroundColor: '#FF6900',
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 15,
+    backgroundColor: "#FFF",
     marginTop: 10,
-    borderRadius: 100, 
+    borderRadius: 8,
   },
-  textButton:{
-    color: '#fff',
-    fontWeight: 'bold',
-  }
+  textButton: {
+    color: "#000",
+    fontWeight: "bold",
+  },
+  textLogout: {
+    textAlign: "center",
+    marginTop: 15,
+    color: "white",
+  },
+  textLogoutCad: {
+    fontWeight: "bold",
+  },
 });
