935f8f9cf565e826f27d606e5821b6c85180487e
ViniHGV <ixxvinicius@gmail.com>
2023-10-10 13:04:58 -0300

    Adicionando Imagem da Evoque na tela inicial

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -7,9 +7,11 @@ import {
   Image,
   TouchableOpacity,
   SafeAreaView,
+  ImageBackground,
 } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
+import { position } from "native-base/lib/typescript/theme/styled-system";
 
 export interface LoginProps {
   navigation: StackNavigationProp<any>;
@@ -30,64 +32,76 @@ export default function Login({ navigation }: LoginProps) {
   return (
     <SafeAreaView style={styles.container}>
       <Image
-        source={require("../../assets/images/evoque.png")}
-        style={styles.image}
-      />
-      <View>
-        <TextInput
-          style={styles.input}
-          placeholder="exemplo@gmail.com"
-          onChangeText={setEmail}
-          value={email}
-          placeholderTextColor={"#fff"}
+        style={{ position: "absolute", opacity: 0.7 }}
+        source={require("../../assets/images/ImgEvoque.png")}
+      ></Image>
+      <View
+        style={{
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={require("../../assets/images/evoque.png")}
+          style={styles.image}
         />
-        <TextInput
-          style={styles.input}
-          placeholder="*******"
-          secureTextEntry
-          onChangeText={setPassword}
-          value={password}
-          placeholderTextColor={"#fff"}
-        />
-        <TouchableOpacity
-          activeOpacity={0.65}
-          style={styles.button}
-          onPress={handleLogin}
-        >
-          <Text style={styles.textButton}>Entrar</Text>
-        </TouchableOpacity>
-        <View style={styles.loginOther}>
-          <View style={styles.viewOu}></View>
-          <Text style={styles.textOU}>OU</Text>
-          <View style={styles.viewOu}></View>
-        </View>
-        <View style={styles.register}>
-          <Text style={styles.textLogout}>Não tem mais conta?</Text>
-          <TouchableOpacity activeOpacity={0.65} onPress={handleRegister}>
-            <Text style={styles.textLogoutCad}>Cadastre-se</Text>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="exemplo@gmail.com"
+            onChangeText={setEmail}
+            value={email}
+            placeholderTextColor={"#fff"}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="*******"
+            secureTextEntry
+            onChangeText={setPassword}
+            value={password}
+            placeholderTextColor={"#fff"}
+          />
+          <TouchableOpacity
+            activeOpacity={0.65}
+            style={styles.button}
+            onPress={handleLogin}
+          >
+            <Text style={styles.textButton}>Entrar</Text>
           </TouchableOpacity>
+          <View style={styles.loginOther}>
+            <View style={styles.viewOu}></View>
+            <Text style={styles.textOU}>OU</Text>
+            <View style={styles.viewOu}></View>
+          </View>
+          <View style={styles.register}>
+            <Text style={styles.textLogout}>Não tem mais conta?</Text>
+            <TouchableOpacity activeOpacity={0.65} onPress={handleRegister}>
+              <Text style={styles.textLogoutCad}>Cadastre-se</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
+
       <StatusBar style="light" translucent />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1E1E1E",
+    alignItems: "center",
+  },
   loginOther: {
     display: "flex",
     flexDirection: "row",
-    alignItems: "center",
     justifyContent: "center",
     gap: 10,
   },
   viewOu: { height: 1, backgroundColor: "white", width: "33%", marginTop: 15 },
-  container: {
-    flex: 1,
-    backgroundColor: "#1E1E1E",
-    justifyContent: "center",
-    alignItems: "center",
-  },
   title: {
     textTransform: "uppercase",
     textAlign: "center",
