b3c37e756c30709d95aec4f4e40da099cbbfcac3
ViniHGV <ixxvinicius@gmail.com>
2023-10-16 09:59:21 -0300

    Adicionando filtragem na sessão de categorias de treino

--- src/screens/Training/index.tsx
+++ src/screens/Training/index.tsx
@@ -16,6 +16,7 @@ export default function Treinos() {
   const [modalOpen, setModalOpen] = React.useState(false);
   const [status, setStatus] = React.useState({});
   const [videoTraining, setVideoTraining] = useState();
+  const [levelTraining, setLevelTraining] = useState("Iniciante");
 
   return (
     <ScrollView style={{ backgroundColor: "#1E1E1E", flex: 1, padding: 20 }}>
@@ -33,85 +34,96 @@ export default function Treinos() {
           }}
         >
           {DataLevelTraining.map((data) => (
-            <View style={{ alignItems: "center" }}>
-              <Image source={data.image} />
-              <Text
-                style={{
-                  fontWeight: "bold",
-                  color: "#FFF",
-                  fontSize: 12,
-                }}
-              >
-                {data.level}
-              </Text>
-            </View>
+            <TouchableOpacity
+              activeOpacity={0.65}
+              onPress={() => setLevelTraining(data.level)}
+            >
+              <View style={{ alignItems: "center" }}>
+                <Image source={data.image} />
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    color: "#FFF",
+                    fontSize: 12,
+                  }}
+                >
+                  {data.level}
+                </Text>
+              </View>
+            </TouchableOpacity>
           ))}
         </View>
       </View>
-      {TrainingData.map((training) => (
-        <View style={{ gap: 9, marginVertical: 20 }}>
-          <Text style={{ fontWeight: "bold", color: "#FFF", fontSize: 14 }}>
-            {training.title}
-          </Text>
-          <View>
-            <ImageBackground
-              style={{
-                height: 153,
-                justifyContent: "center",
-                gap: 10,
-                paddingLeft: 25,
-              }}
-              source={training.image}
-            >
-              <Text
+      {TrainingData.filter((training) => training.level === levelTraining).map(
+        (training) => (
+          <View style={{ gap: 9, marginVertical: 20 }}>
+            <Text style={{ fontWeight: "bold", color: "#FFF", fontSize: 14 }}>
+              {training.title}
+            </Text>
+            <View>
+              <ImageBackground
                 style={{
-                  color: "#CECECE",
-                  fontSize: 14,
-                  fontWeight: "bold",
+                  height: 153,
+                  justifyContent: "center",
+                  gap: 10,
+                  paddingLeft: 25,
                 }}
+                source={training.image}
               >
-                {training.quantityTrainings}
-              </Text>
-              <Text style={{ color: "#FFF", fontSize: 14, fontWeight: "bold" }}>
-                {training.level}
-              </Text>
-            </ImageBackground>
-            {training?.dataTraining?.map((dataTrainings) => (
-              <TouchableOpacity
-                activeOpacity={0.65}
-                onPress={() => {
-                  setModalOpen(true);
-                  setVideoTraining(dataTrainings.videoSource);
-                }}
-              >
-                <View
+                <Text
                   style={{
-                    padding: 10,
-                    alignItems: "center",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
+                    color: "#CECECE",
+                    fontSize: 14,
+                    fontWeight: "bold",
                   }}
                 >
-                  <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    <Image source={dataTrainings.image}></Image>
-                    <View style={{ gap: 10, marginLeft: 14 }}>
-                      <Text style={{ color: "#FFF", fontWeight: "bold" }}>
-                        {`${dataTrainings.name} - ${dataTrainings.level}`}
-                      </Text>
-                      <Text style={{ color: "#FFF" }}>
-                        {dataTrainings.timer}
-                      </Text>
+                  {training.quantityTrainings}
+                </Text>
+                <Text
+                  style={{ color: "#FFF", fontSize: 14, fontWeight: "bold" }}
+                >
+                  {training.level}
+                </Text>
+              </ImageBackground>
+              {training?.dataTraining?.map((dataTrainings) => (
+                <TouchableOpacity
+                  activeOpacity={0.65}
+                  onPress={() => {
+                    setModalOpen(true);
+                    setVideoTraining(dataTrainings.videoSource);
+                  }}
+                >
+                  <View
+                    style={{
+                      padding: 10,
+                      alignItems: "center",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <View
+                      style={{ flexDirection: "row", alignItems: "center" }}
+                    >
+                      <Image source={dataTrainings.image}></Image>
+                      <View style={{ gap: 10, marginLeft: 14 }}>
+                        <Text style={{ color: "#FFF", fontWeight: "bold" }}>
+                          {`${dataTrainings.name} - ${dataTrainings.level}`}
+                        </Text>
+                        <Text style={{ color: "#FFF" }}>
+                          {dataTrainings.timer}
+                        </Text>
+                      </View>
                     </View>
+                    <Image
+                      source={require("../../assets/images/arrowRight.png")}
+                    />
                   </View>
-                  <Image
-                    source={require("../../assets/images/arrowRight.png")}
-                  />
-                </View>
-              </TouchableOpacity>
-            ))}
+                </TouchableOpacity>
+              ))}
+            </View>
           </View>
-        </View>
-      ))}
+        )
+      )}
       <GenericModal
         modalOpen={modalOpen}
         setOpenModal={() => setModalOpen(false)}
